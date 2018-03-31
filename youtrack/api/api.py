6aa01c69bba5fd887e9d02dcbcec2cd81c393509
Oleg Abramov <t0rr@mail.ru>
2018-04-01 00:07:08 +0300

    session close is coro

--- youtrack/api/api.py
+++ youtrack/api/api.py
@@ -27,8 +27,8 @@ class YouTrackAPI:
             connector=aiohttp.TCPConnector(limit=10),
             loop=self.loop, json_serialize=json.dumps)
 
-    def __del__(self):
-        self.session.close()
+    async def close(self):
+        await self.session.close()
 
     async def login(self):
         await self._login(*self._last_credentials)
